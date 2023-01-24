c9d978d842b10edb28fc87d6b12af6db5e157ebe
ashwantmanikoth <ashwanta75@gmail.com>
2023-01-24 17:32:50 -0400

    assignment 1 - changes made

--- Calendar.java
+++ Calendar.java
@@ -5,8 +5,3 @@ public interface Calendar{
     void addEvent(Event event, Date today);
 
 }
-//public class Calendar {
-//    void addEvent(Event event, Date date) {
-//        System.out.println("Adding an event on the given date.");
-//    }
-//}
