047911f84aada31a779bbcd0aa15ee9342fc5fd0
ashwantmanikoth <ashwanta75@gmail.com>
2023-02-19 22:49:27 -0400

    L04_truck addition

---
+++ Lo4_E02/Truck.java
@@ -0,0 +1,14 @@
+package Lo4_E02;
+
+public class Truck extends Vehicle{
+    protected Truck(Workshop w1, Workshop w2) {
+        super(w1, w2);
+    }
+
+    @Override public void manufacture()
+    {
+        System.out.print("Car ");
+        w1.work();
+        w2.work();
+    }
+}
