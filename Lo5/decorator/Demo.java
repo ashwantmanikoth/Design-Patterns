cab833cb0e8f6cdfe4d8d274d1dc12ced6ebdb8c
ashwantmanikoth <ashwanta75@gmail.com>
2023-02-28 13:52:17 -0400

    init

---
+++ Lo5/decorator/Demo.java
@@ -0,0 +1,11 @@
+package Lo5.decorator;
+
+public class Demo {
+    public static void show() {
+        Editor editor = new Editor();
+        editor.openProject("...");
+    }
+    public static void main(String[] args){
+        show();
+    }
+}
